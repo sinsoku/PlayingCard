1a9f79179f42540f275a7b15f8643f655dc4b4d3
sinsoku <sinsoku.listy@gmail.com>
2016-06-28 20:45:14 +0900

    :black_joker: Remove Jocker from Rank

--- Sources/PlayingCard/Rank.swift
+++ Sources/PlayingCard/Rank.swift
@@ -1,5 +1,5 @@
 enum Rank: Int {
-    case Ace
+    case Ace = 1
     case Two
     case Three
     case Four
@@ -12,51 +12,12 @@ enum Rank: Int {
     case Jack
     case Queen
     case King
-    case Joker
 
-    static func fromInt(number: Int) -> Rank? {
-        switch number {
-          case 1: return .Ace
-          case 2: return .Two
-          case 3: return .Three
-          case 4: return .Four
-          case 5: return .Five
-          case 6: return .Six
-          case 7: return .Seven
-          case 8: return .Eight
-          case 9: return .Nine
-          case 10: return .Ten
-          case 11: return .Jack
-          case 12: return .Queen
-          case 13: return .King
-          default: return nil
-        }
-    }
-
-    init?(number: Int) {
+    init?(_ number: Int) {
       if 1...13 ~= number {
-          self = Rank.fromInt(number: number)!
+          self = Rank(rawValue: number)!
       } else {
           return nil
       }
-    }
-
-    func toInt() -> Int? {
-        switch self {
-          case .Ace: return 1
-          case .Two: return 2
-          case .Three: return 3
-          case .Four: return 4
-          case .Five: return 5
-          case .Six: return 6
-          case .Seven: return 7
-          case .Eight: return 8
-          case .Nine: return 9
-          case .Ten: return 10
-          case .Jack: return 11
-          case .Queen: return 12
-          case .King: return 13
-          case .Joker: return nil
-        }
     }
 }
--- Tests/PlayingCard/RankTests.swift
+++ Tests/PlayingCard/RankTests.swift
@@ -2,29 +2,15 @@ import XCTest
 @testable import PlayingCard
 
 class RankTests: XCTestCase {
-    func testFromInt() {
-        XCTAssertEqual(Rank.fromInt(number: 1), Rank.Ace)
-        XCTAssertEqual(Rank.fromInt(number: 13), Rank.King)
-        XCTAssertNil(Rank.fromInt(number: 14))
-    }
-
     func testInitWithNumber() {
-        XCTAssertEqual(Rank(number: 1), Rank.Ace)
-        XCTAssertEqual(Rank(number: 13), Rank.King)
-        XCTAssertNil(Rank(number: 14))
-    }
-
-    func testToInt() {
-        XCTAssertEqual(Rank.Ace.toInt(), 1)
-        XCTAssertEqual(Rank.King.toInt(), 13)
-        XCTAssertNil(Rank.Joker.toInt())
+        XCTAssertEqual(Rank(1), Rank.Ace)
+        XCTAssertEqual(Rank(13), Rank.King)
+        XCTAssertNil(Rank(14))
     }
 
     static var allTests: [(String, (RankTests) -> () throws -> Void)] {
         return [
-            ("testFromInt", testFromInt),
             ("testInitWithNumber", testInitWithNumber),
-            ("testToInt", testToInt),
         ]
     }
 }
