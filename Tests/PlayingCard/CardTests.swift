56243038e22e69082f762e232600ce64278e293c
sinsoku <sinsoku.listy@gmail.com>
2016-07-03 16:47:09 +0900

    :green_heart: Refactor using typealias

--- Tests/PlayingCard/CardTests.swift
+++ Tests/PlayingCard/CardTests.swift
@@ -2,30 +2,32 @@ import XCTest
 @testable import PlayingCard
 
 class CardTests: XCTestCase {
+    typealias CardDc = Card<DefaultComparing>
+
     func testInit() {
-        let card = Card<DefaultComparing>(rank: .Ace, suit: .Spade)
+        let card = CardDc(rank: .Ace, suit: .Spade)
         XCTAssertEqual(card.rank, Rank.Ace)
         XCTAssertEqual(card.suit, Suit.Spade)
     }
 
     func testInitWithString() {
-        XCTAssertEqual(Card<DefaultComparing>("1S"), Card<DefaultComparing>(rank: .Ace, suit: .Spade))
-        XCTAssertEqual(Card<DefaultComparing>("1H"), Card<DefaultComparing>(rank: .Ace, suit: .Heart))
-        XCTAssertEqual(Card<DefaultComparing>("1C"), Card<DefaultComparing>(rank: .Ace, suit: .Club))
-        XCTAssertEqual(Card<DefaultComparing>("1D"), Card<DefaultComparing>(rank: .Ace, suit: .Diamond))
-        XCTAssertEqual(Card<DefaultComparing>("2S"), Card<DefaultComparing>(rank: .Two, suit: .Spade))
+        XCTAssertEqual(CardDc("1S"), CardDc(rank: .Ace, suit: .Spade))
+        XCTAssertEqual(CardDc("1H"), CardDc(rank: .Ace, suit: .Heart))
+        XCTAssertEqual(CardDc("1C"), CardDc(rank: .Ace, suit: .Club))
+        XCTAssertEqual(CardDc("1D"), CardDc(rank: .Ace, suit: .Diamond))
+        XCTAssertEqual(CardDc("2S"), CardDc(rank: .Two, suit: .Spade))
     }
 
     func testEquatable() {
-        let card_1 = Card<DefaultComparing>(rank: .Ace, suit: .Spade)
-        let card_2 = Card<DefaultComparing>(rank: .Ace, suit: .Spade)
+        let card_1 = CardDc(rank: .Ace, suit: .Spade)
+        let card_2 = CardDc(rank: .Ace, suit: .Spade)
         XCTAssertEqual(card_1, card_2)
     }
 
     func testComparable() {
-      let aceOfHeart = Card<DefaultComparing>(rank: .Ace, suit: .Heart)
-      let aceOfSpade = Card<DefaultComparing>(rank: .Ace, suit: .Spade)
-      let twoOfSpade = Card<DefaultComparing>(rank: .Two, suit: .Spade)
+      let aceOfHeart = CardDc(rank: .Ace, suit: .Heart)
+      let aceOfSpade = CardDc(rank: .Ace, suit: .Spade)
+      let twoOfSpade = CardDc(rank: .Two, suit: .Spade)
       XCTAssertGreaterThan(twoOfSpade, aceOfSpade)
       XCTAssertGreaterThan(aceOfSpade, aceOfHeart)
     }
--- Tests/PlayingCard/Game/PokerTests.swift
+++ Tests/PlayingCard/Game/PokerTests.swift
@@ -2,11 +2,13 @@ import XCTest
 @testable import PlayingCard
 
 class PokerTests: XCTestCase {
+    typealias CardPc = Card<Poker.PokerComparing>
+
     func testPokerComparing() {
-        let aceOfSpade = Card<Poker.PokerComparing>(rank: .Ace, suit: .Spade)
-        let aceOfHeart = Card<Poker.PokerComparing>(rank: .Ace, suit: .Heart)
-        let aceOfDiamond = Card<Poker.PokerComparing>(rank: .Ace, suit: .Diamond)
-        let aceOfClub = Card<Poker.PokerComparing>(rank: .Ace, suit: .Club)
+        let aceOfSpade = CardPc(rank: .Ace, suit: .Spade)
+        let aceOfHeart = CardPc(rank: .Ace, suit: .Heart)
+        let aceOfDiamond = CardPc(rank: .Ace, suit: .Diamond)
+        let aceOfClub = CardPc(rank: .Ace, suit: .Club)
 
         XCTAssertGreaterThan(aceOfSpade, aceOfHeart)
         XCTAssertGreaterThan(aceOfSpade, aceOfDiamond)
@@ -15,47 +17,47 @@ class PokerTests: XCTestCase {
         XCTAssertGreaterThan(aceOfHeart, aceOfClub)
         XCTAssertGreaterThan(aceOfDiamond, aceOfClub)
 
-        let twoOfSpade = Card<Poker.PokerComparing>(rank: .Two, suit: .Spade)
+        let twoOfSpade = CardPc(rank: .Two, suit: .Spade)
         XCTAssertGreaterThan(aceOfSpade, twoOfSpade)
 
-        let threeOfSpade = Card<Poker.PokerComparing>(rank: .Three, suit: .Spade)
+        let threeOfSpade = CardPc(rank: .Three, suit: .Spade)
         XCTAssertGreaterThan(threeOfSpade, twoOfSpade)
     }
 
     func testRoyalStraightFlush() {
-        let spadeCards: Set<Card<Poker.PokerComparing>> = [
-          Card<Poker.PokerComparing>(rank: .Ten, suit: .Spade),
-          Card<Poker.PokerComparing>(rank: .Jack, suit: .Spade),
-          Card<Poker.PokerComparing>(rank: .Queen, suit: .Spade),
-          Card<Poker.PokerComparing>(rank: .King, suit: .Spade),
-          Card<Poker.PokerComparing>(rank: .Ace, suit: .Spade)
+        let spadeCards: Set<CardPc> = [
+          CardPc(rank: .Ten, suit: .Spade),
+          CardPc(rank: .Jack, suit: .Spade),
+          CardPc(rank: .Queen, suit: .Spade),
+          CardPc(rank: .King, suit: .Spade),
+          CardPc(rank: .Ace, suit: .Spade)
         ]
         XCTAssertEqual(Poker.Hand.judge(spadeCards), Poker.Hand.RoyalStraightFlush)
 
-        let heartCards: Set<Card<Poker.PokerComparing>> = [
-          Card<Poker.PokerComparing>(rank: .Ten, suit: .Heart),
-          Card<Poker.PokerComparing>(rank: .Jack, suit: .Heart),
-          Card<Poker.PokerComparing>(rank: .Queen, suit: .Heart),
-          Card<Poker.PokerComparing>(rank: .King, suit: .Heart),
-          Card<Poker.PokerComparing>(rank: .Ace, suit: .Heart)
+        let heartCards: Set<CardPc> = [
+          CardPc(rank: .Ten, suit: .Heart),
+          CardPc(rank: .Jack, suit: .Heart),
+          CardPc(rank: .Queen, suit: .Heart),
+          CardPc(rank: .King, suit: .Heart),
+          CardPc(rank: .Ace, suit: .Heart)
         ]
         XCTAssertEqual(Poker.Hand.judge(heartCards), Poker.Hand.RoyalStraightFlush)
 
-        let clubCards: Set<Card<Poker.PokerComparing>> = [
-          Card<Poker.PokerComparing>(rank: .Ten, suit: .Club),
-          Card<Poker.PokerComparing>(rank: .Jack, suit: .Club),
-          Card<Poker.PokerComparing>(rank: .Queen, suit: .Club),
-          Card<Poker.PokerComparing>(rank: .King, suit: .Club),
-          Card<Poker.PokerComparing>(rank: .Ace, suit: .Club)
+        let clubCards: Set<CardPc> = [
+          CardPc(rank: .Ten, suit: .Club),
+          CardPc(rank: .Jack, suit: .Club),
+          CardPc(rank: .Queen, suit: .Club),
+          CardPc(rank: .King, suit: .Club),
+          CardPc(rank: .Ace, suit: .Club)
         ]
         XCTAssertEqual(Poker.Hand.judge(clubCards), Poker.Hand.RoyalStraightFlush)
 
-        let diamondCards: Set<Card<Poker.PokerComparing>> = [
-          Card<Poker.PokerComparing>(rank: .Ten, suit: .Diamond),
-          Card<Poker.PokerComparing>(rank: .Jack, suit: .Diamond),
-          Card<Poker.PokerComparing>(rank: .Queen, suit: .Diamond),
-          Card<Poker.PokerComparing>(rank: .King, suit: .Diamond),
-          Card<Poker.PokerComparing>(rank: .Ace, suit: .Diamond)
+        let diamondCards: Set<CardPc> = [
+          CardPc(rank: .Ten, suit: .Diamond),
+          CardPc(rank: .Jack, suit: .Diamond),
+          CardPc(rank: .Queen, suit: .Diamond),
+          CardPc(rank: .King, suit: .Diamond),
+          CardPc(rank: .Ace, suit: .Diamond)
         ]
         XCTAssertEqual(Poker.Hand.judge(diamondCards), Poker.Hand.RoyalStraightFlush)
     }
