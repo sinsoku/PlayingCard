a8da1e175cd9bb18586539bce8b4346db3b24e1c
sinsoku <sinsoku.listy@gmail.com>
2016-07-09 21:32:54 +0900

    :sparkles: Implement the hand of Poker

--- Sources/PlayingCard/Game/Poker.swift
+++ Sources/PlayingCard/Game/Poker.swift
@@ -25,22 +25,117 @@ class Poker {
       case FourOfAKind
       case StraightFlush
       case RoyalStraightFlush
+      case HighCard
 
-      static func judge<T>(_ cards: Set<Card<T>>) -> Hand? {
+      static func judge<T>(_ cards: Set<Card<T>>) -> Hand {
           if isRoyalStraightFlush(cards) {
             return .RoyalStraightFlush
+          } else if isStraightFlush(cards) {
+            return .StraightFlush
+          } else if isFourOfAKind(cards) {
+            return .FourOfAKind
+          } else if isFullHouse(cards) {
+            return .FullHouse
+          } else if isFlush(cards) {
+            return .Flush
+          } else if isStraight(cards) {
+            return .Straight
+          } else if isThreeOfAKind(cards) {
+            return .ThreeOfAKind
+          } else if isTwoPair(cards) {
+            return .TwoPair
+          } else if isOnePair(cards) {
+            return .OnePair
           } else {
-            return nil
+            return .HighCard
           }
       }
 
-      private static func isRoyalStraightFlush<T>(_ cards: Set<Card<T>>) -> Boolean {
+      private static func isRoyalStraightFlush<T>(_ cards: Set<Card<T>>) -> Bool {
           return [
             Util.factory("10S", "11S", "12S", "13S", "1S"),
             Util.factory("10H", "11H", "12H", "13H", "1H"),
             Util.factory("10C", "11C", "12C", "13C", "1C"),
             Util.factory("10D", "11D", "12D", "13D", "1D")
           ].contains(cards)
+      }
+
+      private static func isStraightFlush<T>(_ cards: Set<Card<T>>) -> Bool {
+          return isFlush(cards) && isStraight(cards)
+      }
+
+      private static func isFourOfAKind<T>(_ cards: Set<Card<T>>) -> Bool {
+          var result: Dictionary<Rank, Int> = [:]
+          cards.forEach {
+              if let value = result[$0.rank] {
+                result[$0.rank] = value + 1
+              } else {
+                result[$0.rank] = 1
+              }
+          }
+          return result.values.contains(4)
+      }
+
+      private static func isFullHouse<T>(_ cards: Set<Card<T>>) -> Bool {
+          var result: Dictionary<Rank, Int> = [:]
+          cards.forEach {
+              if let value = result[$0.rank] {
+                result[$0.rank] = value + 1
+              } else {
+                result[$0.rank] = 1
+              }
+          }
+          return result.values.contains(3) && result.values.contains(2)
+      }
+
+      private static func isFlush<T>(_ cards: Set<Card<T>>) -> Bool {
+        return Set(cards.map { $0.suit }).count == 1
+      }
+
+      private static func isStraight<T>(_ cards: Set<Card<T>>) -> Bool {
+        let ranks = cards.map { $0.rank.rawValue }.sorted()
+
+        if ranks == [1, 10, 11, 12, 13] {
+            return true
+        } else {
+            return ranks[4] - ranks[0] == 4
+        }
+      }
+
+      private static func isThreeOfAKind<T>(_ cards: Set<Card<T>>) -> Bool {
+          var result: Dictionary<Rank, Int> = [:]
+          cards.forEach {
+              if let value = result[$0.rank] {
+                result[$0.rank] = value + 1
+              } else {
+                result[$0.rank] = 1
+              }
+          }
+          return result.values.contains(3)
+      }
+
+      private static func isTwoPair<T>(_ cards: Set<Card<T>>) -> Bool {
+          var result: Dictionary<Rank, Int> = [:]
+          cards.forEach {
+              if let value = result[$0.rank] {
+                result[$0.rank] = value + 1
+              } else {
+                result[$0.rank] = 1
+              }
+          }
+          return result.filter { $1 == 2 }.count == 2
+      }
+
+      private static func isOnePair<T>(_ cards: Set<Card<T>>) -> Bool {
+          var result: Dictionary<Rank, Int> = [:]
+          cards.forEach {
+              if let value = result[$0.rank] {
+                result[$0.rank] = value + 1
+              } else {
+                result[$0.rank] = 1
+              }
+          }
+          return result.filter { $1 == 2 }.count == 1
       }
   }
 }
--- Tests/PlayingCard/Game/PokerTests.swift
+++ Tests/PlayingCard/Game/PokerTests.swift
@@ -38,10 +38,64 @@ class PokerTests: XCTestCase {
         XCTAssertEqual(Poker.Hand.judge(diamondCards), Poker.Hand.RoyalStraightFlush)
     }
 
+    func testStraightFlush() {
+        let cards: Set<CardPc> = Util.factory("3H", "4H", "5H", "6H", "7H")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.StraightFlush)
+    }
+
+    func testFourOfAKind() {
+        let cards: Set<CardPc> = Util.factory("8S", "8H", "8D", "8C", "2D")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.FourOfAKind)
+    }
+
+    func testFullHouse() {
+        let cards: Set<CardPc> = Util.factory("6S", "6H", "6C", "3D", "3C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.FullHouse)
+    }
+
+    func testFlush() {
+        let cards: Set<CardPc> = Util.factory("13C", "10C", "8C", "5C", "2C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.Flush)
+    }
+
+    func testStraight() {
+        let cards: Set<CardPc> = Util.factory("5S", "6D", "7D", "8H", "9C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.Straight)
+    }
+
+    func testThreeOfAKind() {
+        let cards: Set<CardPc> = Util.factory("9S", "9H", "9D", "12S", "7C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.ThreeOfAKind)
+    }
+
+    func testTwoPair() {
+        let cards: Set<CardPc> = Util.factory("12H", "12C", "2H", "2D", "7C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.TwoPair)
+    }
+
+    func testOnePair() {
+        let cards: Set<CardPc> = Util.factory("3D", "3C", "11S", "4H", "1D")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.OnePair)
+    }
+
+    func testHighCard() {
+        let cards: Set<CardPc> = Util.factory("1D", "10S", "9H", "4D", "3C")
+        XCTAssertEqual(Poker.Hand.judge(cards), Poker.Hand.HighCard)
+    }
+
     static var allTests: [(String, (PokerTests) -> () throws -> Void)] {
         return [
             ("testPokerComparing", testPokerComparing),
             ("testRoyalStraightFlush", testRoyalStraightFlush),
+            ("testStraightFlush", testStraightFlush),
+            ("testFourOfAKind", testFourOfAKind),
+            ("testFullHouse", testFullHouse),
+            ("testFlush", testFlush),
+            ("testStraight", testStraight),
+            ("testThreeOfAKind", testThreeOfAKind),
+            ("testTwoPair", testTwoPair),
+            ("testOnePair", testOnePair),
+            ("testHighCard", testHighCard)
         ]
     }
 }
