828cf25a3f2483fe3b75c30678863f4090e4da23
sinsoku <sinsoku.listy@gmail.com>
2016-07-09 18:57:01 +0900

    :recycle: Extract method

--- Sources/PlayingCard/Game/Poker.swift
+++ Sources/PlayingCard/Game/Poker.swift
@@ -27,16 +27,20 @@ class Poker {
       case RoyalStraightFlush
 
       static func judge<T>(_ cards: Set<Card<T>>) -> Hand? {
-          let spadeCards: Set<Card<T>> = Util.factory("10S", "11S", "12S", "13S", "1S")
-          let heartCards: Set<Card<T>> = Util.factory("10H", "11H", "12H", "13H", "1H")
-          let clubCards: Set<Card<T>> = Util.factory("10C", "11C", "12C", "13C", "1C")
-          let diamondCards: Set<Card<T>> = Util.factory("10D", "11D", "12D", "13D", "1D")
-
-          if [spadeCards, heartCards, clubCards, diamondCards].contains(cards) {
+          if isRoyalStraightFlush(cards) {
             return .RoyalStraightFlush
           } else {
             return nil
           }
+      }
+
+      private static func isRoyalStraightFlush<T>(_ cards: Set<Card<T>>) -> Boolean {
+          return [
+            Util.factory("10S", "11S", "12S", "13S", "1S"),
+            Util.factory("10H", "11H", "12H", "13H", "1H"),
+            Util.factory("10C", "11C", "12C", "13C", "1C"),
+            Util.factory("10D", "11D", "12D", "13D", "1D")
+          ].contains(cards)
       }
   }
 }
